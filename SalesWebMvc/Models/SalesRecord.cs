054befea4597725bdedb9205def20403214473cc
Wesley <wesrocha26@gmail.com>
2020-06-30 22:50:50 -0300

    parse portugues

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -9,11 +9,15 @@ namespace SalesWebMvc.Models
         public int Id { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Display(Name ="Data")]
         public DateTime Date { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Display(Name = "Quantidade")]
         public double Amount { get; set; }
+        [Display(Name ="Situação")]
         public SaleStatus Status { get; set; }
+        [Display(Name ="Vendedor")]
         public Seller Seller { get; set; }
 
         public SalesRecord()
--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -11,25 +11,28 @@ namespace SalesWebMvc.Models
 
         [Required(ErrorMessage = "{0} requerido")]
         [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} deve conter entre 1 e 60 caracteres")]
+        [Display(Name = "Nome")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "{0} requerido")]
         [EmailAddress(ErrorMessage = "Digite um e-mail valido")]
         [DataType(DataType.EmailAddress)]
+        [Display(Name = "E-mail")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "{0} requerido")]
-        [Display(Name = "Birth Date")]
+        [Display(Name = "Data de Nascimento")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
         [Required(ErrorMessage = "{0} requerido")]
         [Range(100.0, 50000.0, ErrorMessage = "Valor de salario deve ser entre 100 e 50000")]
-        [Display(Name = "Base Salary")]
+        [Display(Name = "Salário")]
         [DisplayFormat(DataFormatString = "R$ {0:F2}")]
-        public double BaseSalary { get; set; }
+        public double BaseSalary { get; set; }        
         public Department Department { get; set; }
+        [Display(Name = "Departamento")]
         public int DepartmentId { get; set; }
 
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
